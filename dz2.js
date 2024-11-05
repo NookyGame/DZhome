25609f03fc63f509d0e0794218da4f4024e63002
nookygame <garenadvor@gmail.com>
2024-11-05 21:17:46 +0500

    first commit

---
+++ dz2.js
@@ -0,0 +1,17 @@
+// 1 task
+
+let age = prompt('укажи возраст', 0)
+if ( age >= 0 && age < 10){
+    console.log('вы ребенок');
+}
+else if ( age >= 11 && age < 16){
+    console.log('вы подросток');
+}
+else if ( age >= 17 && age < 25){
+    console.log('вы взрослый');
+}
+else if ( age >= 26 && age < 60){
+    console.log('вы пенсионер');
+}
+    
+// 2 task
